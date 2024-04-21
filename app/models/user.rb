e35652e15852f45895d01e822a764b80071a8b0c
mt02v <141305346+mt02v@users.noreply.github.com>
2024-04-21 14:55:06 +0000

    [Add] フレンドリーフォワーディングのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,28 +7,28 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive:false }
   has_secure_password
-  validates :password, presence: true, length: { minimum:6 }
-  
+  validates :password, presence: true, length: { minimum:6 }, allow_nil: true
+
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  
+
   def User.new_token
     SecureRandom.urlsafe_bsase64
   end
-  
+
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(rember_token))
   end
-  
+
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     Bcrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-  
+
   def forget
     update_attribute(:remember_digest, nil)
   end
