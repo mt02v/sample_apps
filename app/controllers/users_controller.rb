c96dbafd0a13e12327dda04c0d7a0daac056d695
mt02v <141305346+mt02v@users.noreply.github.com>
2024-05-19 14:55:00 +0000

    [Add] リスト 13.32:logged_in_userメソッドをApplicationコントローラに移す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,   only: :delete
 
   def index
-    @user = User.where(activated:).paginate(page: params:page])
+    @user = User.where(activated:).paginate(page: params[:page])
   end
 
   def show
